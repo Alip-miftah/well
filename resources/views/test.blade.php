385a9395805e3b38d5f139ac99fcc2704feeefd4
Alip-miftah <alipmiftahfauzi24@gmail.com>
2024-10-22 13:55:38 +0700

    nyobain aja

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -10,8 +10,13 @@
     <div class="card">
         <h1>hallo </h1>
         <h2>nama saya daffa</h2>
-        <h2>okekkk</h2>
+        <h2>okekkk
+            
+        </h2>
     </div>
-    <h1>saya anak rajin</h1>
+    <h1>saya aslinya 2 orang yang satu kabur</h1>
+
+
+        
 </body>
 </html>
